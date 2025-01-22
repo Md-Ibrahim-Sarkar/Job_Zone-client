db313d7de94255181132416a6c293166176a3aa5
Md-Ibrahim-Sarkar <ibrahim06978@gmail.com>
2025-01-22 23:46:43 +0600

    done

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -4,16 +4,14 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { format } from 'date-fns'
 import Swal from 'sweetalert2'
-import useAxios from '../hooks/useAxiosSecure'
 
 
 const MyPostedJobs = () => {
   const user = useAuth()
-  const axiosSecure = useAxios()
   const { data: jobs = [], refetch } = useQuery({
     queryKey: ['jobs', 'email'],
     queryFn: async () => {
-      const response = await axiosSecure(`/jobs/${user?.user?.email}`)
+      const response = await axios(`https://job-zone.vercel.app/jobs/${user?.user?.email}`)
       return response.data
     },
   })
